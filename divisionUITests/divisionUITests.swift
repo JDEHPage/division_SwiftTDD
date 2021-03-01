f88c0a65ec358b7e9f579481b4846d567283c5ff
JohnDPage <john.page@fanduel.com>
2021-03-01 16:12:52 +0000

    Added final UI Tests

--- divisionUITests/divisionUITests.swift
+++ divisionUITests/divisionUITests.swift
@@ -10,34 +10,68 @@ import XCTest
 
 class divisionUITests: XCTestCase {
 
+    var app = XCUIApplication()
+    var device = XCUIDevice.shared
+//    var element = XCUIElement()
+    
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        app.launch()
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
     func testExample() {
         // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
+//        let app = XCUIApplication()
+//        app.launch()
 
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-
-    func testLaunchPerformance() {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
-                XCUIApplication().launch()
-            }
-        }
+    
+    func test_MainScreenLaunch() {
+        let labelText = app.staticTexts["Enter two numbers:"]
+        XCTAssert(labelText.exists, "Should be in the main screen")
+        
     }
+    
+    func test_Calculate10DivideBy2LabelResult() {
+        let div1 = app.textFields.element(boundBy: 0)
+        let div2 = app.textFields.element(boundBy: 1)
+        div1.tap()
+        div1.typeText("10")
+        div2.tap()
+        div2.typeText("2")
+        app.buttons["Calculate"].tap()
+        XCTAssertTrue(app.staticTexts["5.0"].exists, "Result should be displaying '5.0'")
+    }
+    
+    func test_Claculate20DividedBy10LabelResult() {
+        
+        let app = XCUIApplication()
+        app.windows.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .textField).element(boundBy: 0).tap()
+        
+        let moreKey = app/*@START_MENU_TOKEN@*/.keys["more"]/*[[".keyboards",".keys[\"numbers\"]",".keys[\"more\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/
+        moreKey.tap()
+        
+        let key = app/*@START_MENU_TOKEN@*/.keys["2"]/*[[".keyboards.keys[\"2\"]",".keys[\"2\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        key.tap()
+
+        let key2 = app/*@START_MENU_TOKEN@*/.keys["0"]/*[[".keyboards.keys[\"0\"]",".keys[\"0\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        key2.tap()
+
+        app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .textField).element(boundBy: 1).tap()
+        moreKey.tap()
+        
+        let key3 = app/*@START_MENU_TOKEN@*/.keys["1"]/*[[".keyboards.keys[\"1\"]",".keys[\"1\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        key3.tap()
+        key2.tap()
+
+        app/*@START_MENU_TOKEN@*/.staticTexts["Calculate"]/*[[".buttons[\"Calculate\"].staticTexts[\"Calculate\"]",".staticTexts[\"Calculate\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        XCTAssertTrue(app.staticTexts["2.0"].exists, "Result should be displaying '2.0'")
+    }
+
 }
